22dc75047f8c3dff6547371b44657cf19acc6bb1
Davian Smith <davianljsmith@gmail.com>
2022-09-30 11:16:26 -0400

    bash scripts

---
+++ bash_scripts/remove_default_egress.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -x	
+# Remove ingress rule from default VPCs
+
+#Variables
+b=securitygroupid
+c=securityruleid
+
+#Get Region
+for i in `aws ec2 describe-regions | awk {' print $4'}`;
+
+do 
+
+#Get Security Group ID
+    b=`aws ec2 describe-security-groups --filter Name=group-name,Values=default --region ${i} | grep SECURITYGROUPS | awk {'print $6'}`;
+
+#Get Security Rule ID
+    c=`aws ec2 describe-security-group-rules --filter Name="group-id",Values=${b} --region ${i} | grep SECURITYGROUPRULES | grep True | awk {'print $8'}`;
+
+#Delete rule
+    aws ec2 revoke-security-group-egress --group-id ${b} --security-group-rule-ids ${c} --region ${i};
+
+done
